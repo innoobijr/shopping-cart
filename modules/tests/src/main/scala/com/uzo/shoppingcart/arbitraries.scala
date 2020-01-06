017bae21425b60a781c71ea758b0c513021a0f99
Innocent Obi <innocentobi@nodenigeria.com>
2020-01-06 00:11:58 -0600

    with tests

---
+++ modules/tests/src/main/scala/com/uzo/shoppingcart/arbitraries.scala
@@ -0,0 +1,48 @@
+package com.uzo.shoppingcart
+
+import io.estatico.newtype.Coercible
+import io.estatico.newtype.ops._
+import java.util.UUID
+import org.scalacheck.{ Arbitrary, Gen }
+import com.uzo.shoppingcart.domain.brand._
+import com.uzo.shoppingcart.domain.cart._
+import com.uzo.shoppingcart.domain.category._
+import com.uzo.shoppingcart.domain.checkout._
+import com.uzo.shoppingcart.domain.item._
+import com.uzo.shoppingcart.generators._
+import squants.market.Money
+
+object arbitraries {
+    implicit def arbCoercibleInt[A: Coercible[Int, *]]: Arbitrary[A] =
+    Arbitrary(Gen.posNum[Int].map(_.coerce[A]))
+
+  implicit def arbCoercibleStr[A: Coercible[String, *]]: Arbitrary[A] =
+    Arbitrary(cbStr[A])
+
+  implicit def arbCoercibleUUID[A: Coercible[UUID, *]]: Arbitrary[A] =
+    Arbitrary(cbUuid[A])
+
+  implicit val arbBrand: Arbitrary[Brand] =
+    Arbitrary(brandGen)
+
+  implicit val arbCategory: Arbitrary[Category] =
+    Arbitrary(categoryGen)
+
+  implicit val arbMoney: Arbitrary[Money] =
+    Arbitrary(genMoney)
+
+  implicit val arbItem: Arbitrary[Item] =
+    Arbitrary(itemGen)
+
+  implicit val arbCartItem: Arbitrary[CartItem] =
+    Arbitrary(cartItemGen)
+
+  implicit val arbCartTotal: Arbitrary[CartTotal] =
+    Arbitrary(cartTotalGen)
+
+  implicit val arbCart: Arbitrary[Cart] =
+    Arbitrary(cartGen)
+
+  implicit val arbCard: Arbitrary[Card] =
+    Arbitrary(cardGen)
+}

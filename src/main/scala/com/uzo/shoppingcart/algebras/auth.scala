0bd764ec47b8b07c0fe5f65d843acaaa13d0943b
Innocent Obi <innocentobi@nodenigeria.com>
2020-01-05 20:24:58 -0600

    finished with interpreters

--- src/main/scala/com/uzo/shoppingcart/algebras/auth.scala
+++ src/main/scala/com/uzo/shoppingcart/algebras/auth.scala
@@ -1,8 +1,24 @@
 package com.uzo.shoppingcart.algebras
 
+import cats._
+import cats.effect.Sync
+import cats.implicits._
 import com.uzo.shoppingcart.domain.auth.{Password, UserName}
-import com.uzo.shoppingcart.http.auth.users.User
+import com.uzo.shoppingcart.http.auth.users._
+import dev.profunktor.redis4cats.algebra.RedisCommands
 import dev.profunktor.auth.jwt.JwtToken
+import pdi.jwt.JwtClaim
+import com.uzo.shoppingcart.http.auth.users.CommonUser
+import com.uzo.shoppingcart.http.auth.users.AdminUser
+import com.uzo.shoppingcart.http.auth.users
+import com.uzo.shoppingcart.http.json._
+import com.uzo.shoppingcart.effects._
+import io.circe.syntax._
+import io.circe.parser.decode
+import com.uzo.shoppingcart.config.data.TokenExpiration
+import com.uzo.shoppingcart.domain.auth
+import com.uzo.shoppingcart.domain.auth.UserNameInUse
+import com.uzo.shoppingcart.domain.auth.InvalidUserOrPassword
 
 
 /**
@@ -14,8 +30,129 @@ import dev.profunktor.auth.jwt.JwtToken
   * @tparam F
   */
 trait Auth[F[_]]{
-  def findUser(token: JwtToken): F[Option[User]]
   def newUser(username: UserName, password: Password): F[JwtToken]
   def login(username: UserName, password: Password): F[JwtToken]
   def logout(token: JwtToken, username: UserName): F[Unit]
+}
+
+trait Tokens[F[_]] {
+  def create: F[JwtToken]
+  }
+
+
+// Specialized in retrieving a specific kind of user, indicated by second parameter A
+trait UsersAuth[F[_], A]{
+  def findUser(token: JwtToken)(claim: JwtClaim): F[Option[A]]
+}
+
+class LiveUsersAuth[F[_]: Functor](
+  redis: RedisCommands[F, String, String]
+) extends UsersAuth[F, CommonUser]{
+
+  /**
+   * Our function tried to find the user by token in Redis and if there is a result, it tried ot 
+   * decode the JSON as the desired User type. A token is persisted as a simple key, with its value 
+   * being the serialized user in JSON format.
+   * 
+   */ 
+  def findUser(token: JwtToken)(claim: JwtClaim): F[Option[CommonUser]] = 
+  redis
+    .get(token.value)
+    .map(_.flatMap{ u =>
+      decode[User](u).toOption.map(CommonUser.apply)
+    })
+}
+
+// Interpreter for AdminUser
+class LiveAdminAuth[F[_]: Applicative](
+  adminToken: JwtToken,
+  adminUser: AdminUser
+) extends UsersAuth[F, AdminUser]{
+
+  /**
+   * It compare the token with the unique AdminToken that has been passed ot the interpreter
+   * on initialization, and in case of match, it returns the adminUser stored in memeory.
+   */
+  def findUser(token: JwtToken)(claim: JwtClaim): F[Option[users.AdminUser]] = 
+    (token == adminToken)
+      .guard[Option]
+      .as(adminUser)
+      .pure[F]
+}
+object LiveUsersAuth {
+  def make[F[_]: Sync](
+    redis: RedisCommands[F, String, String]
+  ): F[UsersAuth[F, CommonUser]] = 
+    Sync[F].delay(
+      new LiveUsersAuth(redis)
+    )
+}
+
+object LiveAdminAuth {
+  def make[F[_]: Sync](
+    adminToken: JwtToken,
+    adminUser: AdminUser
+  ): F[UsersAuth[F, AdminUser]] = 
+  Sync[F].delay(
+    new LiveAdminAuth(adminToken, adminUser)
+  )
+}
+
+final class LiveAuth[F[_]: GenUUID: MonadThrow] private(
+  tokenExpiration: TokenExpiration,
+  tokens: Tokens[F],
+  users: Users[F],
+  redis: RedisCommands[F, String, String]
+) extends Auth[F]{
+  private val TokenExpiration = tokenExpiration.value
+
+  /**
+   * We try to find the user in PostGres. If it doesn exist, we proceed with its creation
+   * otherwise, we raise a `UserNameInUserError`.
+   *  Create a user means persisiting it in Postgres, create a JWT Token, serializing the user as 
+   * JSON and persisting both the token and the serialized user in Redis for fast access, indicating
+   * an expiration time. 
+   */ 
+  def newUser(username: auth.UserName, password: auth.Password): F[JwtToken] =
+    users.find(username, password).flatMap{
+      case Some(_) => UserNameInUse(username).raiseError[F, JwtToken]
+      case None => 
+        for {
+          i <- users.create(username, password)
+          t <- tokens.create
+          u = User(i,username).asJson.noSpaces
+          _ <- redis.setEx(t.value, u, TokenExpiration)
+          _ <- redis.setEx(username.value, t.value, TokenExpiration)
+        } yield t
+    }
+  
+    def login(username: auth.UserName, password: auth.Password): F[JwtToken] =
+      users.find(username, password).flatMap{
+        case None => InvalidUserOrPassword(username).raiseError[F, JwtToken]
+        case Some(user) =>
+          redis.get(username.value).flatMap{
+            case Some(t) => JwtToken(t).pure[F]
+            case None =>
+            tokens.create.flatTap { t =>
+              redis.setEx(t.value, user.asJson.noSpaces, TokenExpiration) *>
+              redis.setEx(username.value, t.value, TokenExpiration)
+              }
+          }
+      }
+
+    def logout(token: JwtToken, username: auth.UserName): F[Unit] = 
+      redis.del(token.value) *> redis.del(username.value)
+}
+  //Smart constructor
+  object LiveAuth {
+      def make[F[_]: Sync](
+        tokenExpiration: TokenExpiration,
+        tokens: Tokens[F],
+        users: Users[F],
+        redis: RedisCommands[F, String, String]
+      ): F[Auth[F]] = 
+        Sync[F].delay(
+          new LiveAuth(tokenExpiration, tokens, users, redis)
+        )
+  
 }
--- src/main/scala/com/uzo/shoppingcart/algebras/cart.scala
+++ src/main/scala/com/uzo/shoppingcart/algebras/cart.scala
@@ -1,8 +1,18 @@
 package com.uzo.shoppingcart.algebras
 
-import com.uzo.shoppingcart.domain.auth.UserId
-import com.uzo.shoppingcart.domain.cart.{Cart, CartTotal, Quantity}
-import com.uzo.shoppingcart.domain.item.ItemId
+import cats.effect.Sync
+import cats.implicits._
+import com.uzo.shoppingcart.domain.auth._
+import com.uzo.shoppingcart.domain.cart._
+import com.uzo.shoppingcart.domain.item._
+import com.uzo.shoppingcart.effects._
+import squants.market._
+import com.uzo.shoppingcart.config.data.ShoppingCartExpiration
+import dev.profunktor.redis4cats.algebra.RedisCommands
+import com.uzo.shoppingcart.domain.auth
+import com.uzo.shoppingcart.domain.item
+
+
 
 trait ShoppingCart[F[_]]{
   def add(userId: UserId, itemId: ItemId, quantity: Quantity): F[Unit]
@@ -10,4 +20,91 @@ trait ShoppingCart[F[_]]{
   def delete(userId: UserId): F[Unit]
   def removeItem(userId: UserId, itemId: ItemId): F[Unit]
   def update(userId: UserId, cart: Cart): F[Unit]
+}
+
+// safe constrcutor 
+object LiveShoppingCart {
+  def make[F[_]: Sync](
+      items: Items[F],
+      redis: RedisCommands[F, String, String],
+      exp: ShoppingCartExpiration
+  ): F[ShoppingCart[F]] =
+    Sync[F].delay(
+      new LiveShoppingCart(items, redis, exp)
+    )
+}
+
+//Shopping Cart interpreter
+final class LiveShoppingCart[F[_]: GenUUID: MonadThrow] private (
+  items: Items[F],
+  redis: RedisCommands[F, String, String],
+  exp: ShoppingCartExpiration
+) extends ShoppingCart[F]{
+
+  def add(
+    userId: UserId, 
+    itemId: ItemId,
+    quantity: Quantity
+  ): F[Unit] = 
+    redis.hSet(
+      userId.value.toString,
+      itemId.value.toString,
+      quantity.value.toString
+    ) *> 
+    redis.expire(
+      userId.value.toString,
+      exp.value
+    )
+
+  /**
+   *  Tries to find the shopping cart of the user using the hGetAll function, which returns
+   * a `Map[K, V]`. It is exists it parses both filed and vlaues into a List[CartItem] and finally.
+   * It calculates the totla amount. GenUUID[F].read takes a String and returns an F[A] where A: 
+   * Coercible[UUID, *]], in this case, ItemId.
+   * 
+   */
+  def get(userId: UserId): F[CartTotal] = 
+    redis.hGetAll(userId.value.toString).flatMap{ it =>
+      it.toList
+        .traverseFilter{
+          case (k, v) => 
+            for {
+              id <- GenUUID[F].read[ItemId](k)
+              qt <- ApThrow[F].catchNonFatal(Quantity(v.toInt))
+              rs <- items.findById(id).map(_.map(i => CartItem(i, qt)))
+            } yield rs
+        }
+        .map(items => CartTotal(items, calcTotal(items)))
+    }
+
+    private def calcTotal(items: List[CartItem]): Money = 
+    USD(items
+      .foldMap{ i =>
+        i.item.price.value * i.quantity.value
+      }
+    )
+
+    def delete(userId: auth.UserId): F[Unit] = redis.del(userId.valid.toString)
+
+    def removeItem(userId: auth.UserId, itemId: item.ItemId): F[Unit] = 
+      redis.hDel(userId.value.toString, itemId.value.toString)
+
+    /**
+     * Retrieves the shoppign cart for the user (if it exists) and it updates teh quantituy
+     * of each matching item, following by updating the shopping cart expiration.
+     */
+    def update(userId: auth.UserId, cart: Cart): F[Unit] = 
+      redis.hGetAll(userId.value.toString).flatMap{it =>
+        it.toList.traverse_ {
+          case (k, _) => 
+            GenUUID[F].read[ItemId](k).flatMap{ id =>
+              cart.items.get(id).traverse_ { q =>
+                redis.hSet(userId.value.toString, k, q.value.toString)
+              }
+            }
+          } *>
+            redis.expire(userId.value.toString, exp.value)
+          }
+
+    
 }
